6de3aa6835846adab33eb726757f711d19e9f911
Karthik Saxena <katik.ks@gmail.com>
2021-01-06 20:28:11 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,91 @@
+// console.log('This is JS');
+
+// alert('This is from App.js file');
+
+//  let result = prompt('Your Name');
+
+//  console.log(result);
+
+
+// let a = 5;
+
+// console.log(a);
+
+let a = 5;
+
+console.log(typeof(a));
+
+let str = 'Hello';
+
+console.log(str);
+
+console.log(typeof(str));
+
+let isApproved = false;
+
+console.log(typeof(isApproved));
+
+
+let firstName = undefined;
+
+console.log(typeof(firstName));
+
+
+
+let lastName = null;
+
+console.log(typeof(lastName));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
